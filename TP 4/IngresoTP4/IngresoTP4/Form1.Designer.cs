dcb35bf588d217cde2871d27bb061e579986e891
Brenda Yañez <103775472+brendayw@users.noreply.github.com>
2023-07-05 18:49:57 -0300

    Add files via upload

---
+++ TP 4/IngresoTP4/IngresoTP4/Form1.Designer.cs
@@ -0,0 +1,188 @@
+﻿
+namespace IngresoTP4
+{
+    partial class FIngreso
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.labelNombres = new System.Windows.Forms.Label();
+            this.labelApellidos = new System.Windows.Forms.Label();
+            this.labelemail = new System.Windows.Forms.Label();
+            this.labelcontra = new System.Windows.Forms.Label();
+            this.labelconfirma = new System.Windows.Forms.Label();
+            this.tNombres = new System.Windows.Forms.TextBox();
+            this.tApellidos = new System.Windows.Forms.TextBox();
+            this.tEmail = new System.Windows.Forms.TextBox();
+            this.tContrasenia = new System.Windows.Forms.TextBox();
+            this.tConfirmacion = new System.Windows.Forms.TextBox();
+            this.bAceptar = new System.Windows.Forms.Button();
+            this.bCancelar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // labelNombres
+            // 
+            this.labelNombres.AutoSize = true;
+            this.labelNombres.Location = new System.Drawing.Point(103, 42);
+            this.labelNombres.Name = "labelNombres";
+            this.labelNombres.Size = new System.Drawing.Size(52, 13);
+            this.labelNombres.TabIndex = 0;
+            this.labelNombres.Text = "Nombres:";
+            // 
+            // labelApellidos
+            // 
+            this.labelApellidos.AutoSize = true;
+            this.labelApellidos.Location = new System.Drawing.Point(103, 79);
+            this.labelApellidos.Name = "labelApellidos";
+            this.labelApellidos.Size = new System.Drawing.Size(52, 13);
+            this.labelApellidos.TabIndex = 1;
+            this.labelApellidos.Text = "Apellidos:";
+            // 
+            // labelemail
+            // 
+            this.labelemail.AutoSize = true;
+            this.labelemail.Location = new System.Drawing.Point(120, 116);
+            this.labelemail.Name = "labelemail";
+            this.labelemail.Size = new System.Drawing.Size(35, 13);
+            this.labelemail.TabIndex = 2;
+            this.labelemail.Text = "Email:";
+            // 
+            // labelcontra
+            // 
+            this.labelcontra.AutoSize = true;
+            this.labelcontra.Location = new System.Drawing.Point(91, 153);
+            this.labelcontra.Name = "labelcontra";
+            this.labelcontra.Size = new System.Drawing.Size(64, 13);
+            this.labelcontra.TabIndex = 3;
+            this.labelcontra.Text = "Contraseña:";
+            // 
+            // labelconfirma
+            // 
+            this.labelconfirma.AutoSize = true;
+            this.labelconfirma.Location = new System.Drawing.Point(84, 190);
+            this.labelconfirma.Name = "labelconfirma";
+            this.labelconfirma.Size = new System.Drawing.Size(71, 13);
+            this.labelconfirma.TabIndex = 4;
+            this.labelconfirma.Text = "Confirmacion:";
+            // 
+            // tNombres
+            // 
+            this.tNombres.Location = new System.Drawing.Point(188, 35);
+            this.tNombres.Name = "tNombres";
+            this.tNombres.Size = new System.Drawing.Size(204, 20);
+            this.tNombres.TabIndex = 5;
+            // 
+            // tApellidos
+            // 
+            this.tApellidos.Location = new System.Drawing.Point(188, 77);
+            this.tApellidos.Name = "tApellidos";
+            this.tApellidos.Size = new System.Drawing.Size(204, 20);
+            this.tApellidos.TabIndex = 6;
+            // 
+            // tEmail
+            // 
+            this.tEmail.Location = new System.Drawing.Point(188, 117);
+            this.tEmail.Name = "tEmail";
+            this.tEmail.Size = new System.Drawing.Size(204, 20);
+            this.tEmail.TabIndex = 7;
+            // 
+            // tContrasenia
+            // 
+            this.tContrasenia.Location = new System.Drawing.Point(188, 152);
+            this.tContrasenia.Name = "tContrasenia";
+            this.tContrasenia.PasswordChar = '*';
+            this.tContrasenia.Size = new System.Drawing.Size(204, 20);
+            this.tContrasenia.TabIndex = 8;
+            // 
+            // tConfirmacion
+            // 
+            this.tConfirmacion.Location = new System.Drawing.Point(188, 190);
+            this.tConfirmacion.Name = "tConfirmacion";
+            this.tConfirmacion.PasswordChar = '*';
+            this.tConfirmacion.Size = new System.Drawing.Size(204, 20);
+            this.tConfirmacion.TabIndex = 9;
+            // 
+            // bAceptar
+            // 
+            this.bAceptar.Location = new System.Drawing.Point(222, 276);
+            this.bAceptar.Name = "bAceptar";
+            this.bAceptar.Size = new System.Drawing.Size(108, 34);
+            this.bAceptar.TabIndex = 10;
+            this.bAceptar.Text = "&Aceptar";
+            this.bAceptar.UseVisualStyleBackColor = true;
+            this.bAceptar.Click += new System.EventHandler(this.bAceptar_Click);
+            // 
+            // bCancelar
+            // 
+            this.bCancelar.Location = new System.Drawing.Point(350, 276);
+            this.bCancelar.Name = "bCancelar";
+            this.bCancelar.Size = new System.Drawing.Size(108, 34);
+            this.bCancelar.TabIndex = 11;
+            this.bCancelar.Text = "&Cancelar";
+            this.bCancelar.UseVisualStyleBackColor = true;
+            this.bCancelar.Click += new System.EventHandler(this.bCancelar_Click);
+            // 
+            // FIngreso
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(470, 322);
+            this.Controls.Add(this.bCancelar);
+            this.Controls.Add(this.bAceptar);
+            this.Controls.Add(this.tConfirmacion);
+            this.Controls.Add(this.tContrasenia);
+            this.Controls.Add(this.tEmail);
+            this.Controls.Add(this.tApellidos);
+            this.Controls.Add(this.tNombres);
+            this.Controls.Add(this.labelconfirma);
+            this.Controls.Add(this.labelcontra);
+            this.Controls.Add(this.labelemail);
+            this.Controls.Add(this.labelApellidos);
+            this.Controls.Add(this.labelNombres);
+            this.Name = "FIngreso";
+            this.Text = "Ingreso";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelNombres;
+        private System.Windows.Forms.Label labelApellidos;
+        private System.Windows.Forms.Label labelemail;
+        private System.Windows.Forms.Label labelcontra;
+        private System.Windows.Forms.Label labelconfirma;
+        private System.Windows.Forms.TextBox tNombres;
+        private System.Windows.Forms.TextBox tApellidos;
+        private System.Windows.Forms.TextBox tEmail;
+        private System.Windows.Forms.TextBox tContrasenia;
+        private System.Windows.Forms.TextBox tConfirmacion;
+        private System.Windows.Forms.Button bAceptar;
+        private System.Windows.Forms.Button bCancelar;
+    }
+}
+
---
+++ TP 4/IngresoTP4/IngresoTP4/Form1.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace IngresoTP4
+{
+    public partial class FIngreso : Form
+    {
+        public FIngreso()
+        {
+            InitializeComponent();
+        }
+
+
+        private void bCancelar_Click(object sender, EventArgs e)
+        {
+            DialogResult respuesta;
+
+            respuesta = MessageBox.Show("Desea salir?", "Cerrar",
+                MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+
+            if (respuesta == DialogResult.Yes)
+            {
+                Close();
+            }
+            else
+            {
+
+            }
+        }
+
+        private void bAceptar_Click(object sender, EventArgs e)
+        {
+            string nombre = tNombres.Text;
+            string apellido = tApellidos.Text;
+            string contra = tContrasenia.Text;
+            string confirma = tConfirmacion.Text;
+            string email = tEmail.Text;
+
+            //bools del email
+            Boolean arroba = email.Contains("@");
+            Boolean dot = email.Contains(".");
+
+
+            //bools de la contraseña
+            Boolean numero = false;
+            Boolean letra = false;
+            Boolean simbolo = false;
+
+            int n = nombre.Length;
+            int a = apellido.Length;
+            int c = contra.Length;
+            int cf = confirma.Length;
+            int em = email.Length;
+
+
+            while ( n <= 0 || a <= 0 || c <=0 || cf <= 0 || em <= 0 )
+            {
+                MessageBox.Show("Hay campos sin rellenar", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                break;
+
+            }
+
+            
+
+
+            if ((nombre != "") && (apellido != "") && (contra != "") && (confirma != "") && (email != ""))
+            {
+                
+                while (!arroba || !dot) //se muestra igual aunque tenga el arroba
+                {
+                    MessageBox.Show("Ingrese un email valido", "Email no valido", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    tEmail.Focus();
+                    break;
+                }
+
+                while (contra != confirma || c < 8)
+                {
+                    if (contra != confirma)
+                    {
+                        MessageBox.Show("Las contraseñas no coinciden", "Contraseñas no validas", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        tConfirmacion.Focus();
+                    } else
+                    {
+                        MessageBox.Show("La contraseña debe tener minimo 8 caracteres", "Contraseña no valida", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        tContrasenia.Focus();
+                    }
+                    break;
+                    
+                }
+
+
+                foreach (char caracter in contra)
+                {
+                    if (char.IsLetter(caracter) && char.IsDigit(caracter) && !char.IsLetterOrDigit(caracter))
+
+                        MessageBox.Show("La contraseña debe contener al menos una letra, un numero y un simbolo", 
+                            "Contraseña no valida", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    tContrasenia.Focus();
+                }
+
+            } 
+            
+            if ((nombre != "") && (apellido != "") && (contra != "") && (confirma != "") && (email != "") && (contra == confirma) && c >= 8 && !numero && !letra && !simbolo)
+            {
+                
+              /*  char inicialNombre = nombre[0], inicialApellido = apellido[0];*/
+
+                string [] inicialesNombre = nombre.Split(' '), inicialesApellido = apellido.Split(' ');
+
+                string primerN = nombre[0];
+                string segundoN = nombre[1];
+                char inicialNombre = primerN[0], inicialNombreS = segundoN[0];
+
+                string primerA = apellido[a - 1];
+                char inicialApellido = primerA[0];
+                
+
+                MessageBox.Show($"Ingreso exitoso \n " +
+                                $"Su usuario asignado es: {inicialNombre}{inicialApellido}\n" + //falta saber si son compuestos
+                                $" ", "Exitoso", MessageBoxButtons.OK); /*asignar usuario*/
+            
+            }
+
+        }
+    }
+}
