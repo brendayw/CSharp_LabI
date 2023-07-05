01442d4ff67676f917639639579bb0d6f4642754
Brenda Yañez <103775472+brendayw@users.noreply.github.com>
2023-07-05 18:28:18 -0300

    Add files via upload

---
+++ TP 1/CalcularVueltotp1/NumeroNaturalesInvertidostp1/NumeroNaturalesInvertidostp1/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NumeroNaturalesInvertidostp1
+{
+    public partial class FNumerosNaturales : Form
+    {
+        public FNumerosNaturales()
+        {
+            InitializeComponent();
+        }
+
+        double a, b, aux;
+
+        private void binvertir_Click(object sender, EventArgs e)
+        {
+            a = Convert.ToDouble(tnumeroA.Text);
+            b = Convert.ToDouble(tNumeroB.Text);
+
+            aux = a;
+            a = b;
+            b = aux;
+
+            MessageBox.Show("Los números invertidos son: \n A = " + a + " \n B = " + b + " ");
+
+        }
+    }
+}
